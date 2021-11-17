98856e61eb2f26e8b9dcf59766747cf30aeea189
Tadej <tblasko1984@gmail.com>
2021-11-17 16:47:40 +0100

    GIT

---
+++ src/five/ContinueWithLabelDemo.java
@@ -0,0 +1,29 @@
+package five;
+
+/**
+ * String rečenica = "Pronađi mene ti"
+ * String riječ = "men"
+ */
+
+//public class ContinueWithLabelDemo {
+//    public static void main(String[] args) {
+//        String rečenica = "Pronađi mene ti";
+//        String riječ = "men";
+////        char[] rečenicaNiz = rečenica.toCharArray();
+////        char[] riječNiz = riječ.toCharArray();
+//        int max = rečenica.length() - riječ.length();
+//        boolean foundIt = false;
+//        NAŠA:
+//        for (int i = 0; i < max; i++) {
+//            for (int j = 0; j < riječNiz.length; j++) {
+//                char slovoRečenica = rečenicaNiz[i + j];
+//                char slovoRiječi = riječNiz[j];
+//                if (slovoRečenica != slovoRiječi) {
+//                    continue NAŠA;
+//                }
+//            }
+//            foundIt = true;
+//            break;
+//        }
+//    }
+//}
